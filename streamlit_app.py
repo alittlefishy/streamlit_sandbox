fef2e49d9744ca8b1670c4a92657c65b08c92036
Tony Lui <alittlefishy@users.noreply.github.com>
2025-04-17 02:35:16 +0000

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title('🍡 Data Dango')
 
 st.write('Hello world!')
+
+x = st.text_input("Movie", "Star Wars")
+
+if st.button("Click Me"):
+    st.write(f"Your favorite movie is `{x}`")
